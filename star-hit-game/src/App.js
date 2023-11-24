b62e2ee8132909ecded66f94fd21904f0bb220d7
githubdemo00 <151752950+githubdemo00@users.noreply.github.com>
2023-11-24 14:21:16 +0000

    add collection of email and store in cosmosdb

--- star-hit-game/src/App.js
+++ star-hit-game/src/App.js
@@ -4,16 +4,56 @@ import Game from './Game';
 import TicTacToe from './TicTacToe';
 import StarWars from './StarWars'; // Import the StarWars component
 
+const { CosmosClient } = require('@azure/cosmos');
+
 const App = () => {
     const [game, setGame] = useState(null);
+    const [name, setName] = useState(''); // New state variable for the name
+    const [email, setEmail] = useState(''); // New state variable for the email
 
     const handleGameSelection = (selectedGame) => {
         setGame(selectedGame);
     };
 
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsSubmitted(true);
+
+        const client = new CosmosClient({ endpoint: process.env.COSMOS_DB_ENDPOINT, key: process.env.COSMOS_DB_KEY });
+        const database = client.database(process.env.COSMOS_DB_DATABASE);
+        const container = database.container(process.env.COSMOS_DB_CONTAINER);
+        const { resource: createdItem } = await container.items.create({ name, email });
+    };
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:@"]+(\.[^<>()[\]\\.,;:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
     return (
         <div className="app-container">
-            {!game && (
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={name} onChange={handleNameChange} required />
+                </label>
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={handleEmailChange} required />
+                </label>
+                <input type="submit" value="Submit" disabled={!name || !validateEmail(email)} />
+            </form>
+            {isSubmitted && name && validateEmail(email) && (
                 <div className="game-selection">
                     <div className="game-icon">🕹️</div>
                     <h1 className="game-title">Choose Your Game</h1>
